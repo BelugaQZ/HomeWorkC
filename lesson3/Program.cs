324788e84b0014aa37264dd92ff6dc211c51cb1a
Beluga <belugaqz@gmail.com>
2023-03-22 21:17:13 +0500

    lesson 3 доработка

--- lesson3/Program.cs
+++ lesson3/Program.cs
@@ -8,7 +8,7 @@ while(s.Length != 5) {
     s = Console.ReadLine()!;
 }
 
-if(s[0] == s[4] || s[1] == s[3]) {
+if(s[0] == s[4] && s[1] == s[3]) {
     Console.Write("Число является палиндромом");
 } else {
     Console.Write("Число не является палиндромом");
